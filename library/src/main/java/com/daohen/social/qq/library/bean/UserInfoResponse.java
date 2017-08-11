2cc8737691f834f20214d07f718285559455fb8f
alun <alunfeixue@qq.com>
2017-08-11 15:12:56 +0800

    commit

--- library/src/main/java/com/daohen/social/qq/library/bean/UserInfoResponse.java
+++ library/src/main/java/com/daohen/social/qq/library/bean/UserInfoResponse.java
@@ -20,116 +20,75 @@ public class UserInfoResponse {
     private String is_yellow_vip;
     private String gender;
     private String figureurl;
+    private int is_lost;
+    private String province;
+    private String city;
 
-    public String getIsYellowYearVip() {
+    public String getIs_yellow_year_vip() {
         return is_yellow_year_vip;
-    }
-
-    public void setIsYellowYearVip(String is_yellow_year_vip) {
-        this.is_yellow_year_vip = is_yellow_year_vip;
     }
 
     public int getRet() {
         return ret;
     }
 
-    public void setRet(int ret) {
-        this.ret = ret;
-    }
-
-    public String getFigureurlQq1() {
+    public String getFigureurl_qq_1() {
         return figureurl_qq_1;
     }
 
-    public void setFigureurlQq1(String figureurl_qq_1) {
-        this.figureurl_qq_1 = figureurl_qq_1;
-    }
-
-    public String getFigureurlQq2() {
+    public String getFigureurl_qq_2() {
         return figureurl_qq_2;
-    }
-
-    public void setFigureurlQq2(String figureurl_qq_2) {
-        this.figureurl_qq_2 = figureurl_qq_2;
     }
 
     public String getNickname() {
         return nickname;
     }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getYellowVipLevel() {
+    public String getYellow_vip_level() {
         return yellow_vip_level;
-    }
-
-    public void setYellowVipLevel(String yellow_vip_level) {
-        this.yellow_vip_level = yellow_vip_level;
     }
 
     public String getMsg() {
         return msg;
     }
 
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public String getFigureurl1() {
+    public String getFigureurl_1() {
         return figureurl_1;
-    }
-
-    public void setFigureurl1(String figureurl_1) {
-        this.figureurl_1 = figureurl_1;
     }
 
     public String getVip() {
         return vip;
     }
 
-    public void setVip(String vip) {
-        this.vip = vip;
-    }
-
     public String getLevel() {
         return level;
     }
 
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getFigureurl2() {
+    public String getFigureurl_2() {
         return figureurl_2;
     }
 
-    public void setFigureurl2(String figureurl_2) {
-        this.figureurl_2 = figureurl_2;
-    }
-
-    public String getIsYellowVip() {
+    public String getIs_yellow_vip() {
         return is_yellow_vip;
-    }
-
-    public void setIsYellowVip(String is_yellow_vip) {
-        this.is_yellow_vip = is_yellow_vip;
     }
 
     public String getGender() {
         return gender;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     public String getFigureurl() {
         return figureurl;
     }
 
-    public void setFigureurl(String figureurl) {
-        this.figureurl = figureurl;
+    public int getIs_lost() {
+        return is_lost;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getCity() {
+        return city;
     }
 }
