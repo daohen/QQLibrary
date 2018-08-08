e1770b532baf6afe0fb061c53f526cda4f9fe427
daohen <daohen>
2018-08-08 18:24:52 +0800

    commit

---
+++ library/src/main/java/com/daohen/social/qq/library/listener/DefaultIUiListener.java
@@ -0,0 +1,26 @@
+package com.daohen.social.qq.library.listener;
+
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.UiError;
+
+/**
+ * CREATE BY ALUN
+ * EMAIL: alunfeixue2011@gmail.com
+ * DATE : 2018/08/08 18:23
+ */
+public class DefaultIUiListener implements IUiListener {
+    @Override
+    public void onComplete(Object o) {
+
+    }
+
+    @Override
+    public void onError(UiError uiError) {
+
+    }
+
+    @Override
+    public void onCancel() {
+
+    }
+}
